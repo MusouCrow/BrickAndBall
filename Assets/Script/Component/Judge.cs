4f68ae3fa3d385de3f0f12892bf60fdb361cd3b5
MusouCrow <939549924@qq.com>
2017-12-10 19:20:12 +0800

    Make [Game.Component.Wall].

---
+++ Assets/Script/Component/Judge.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Component {
+	public class Judge : MonoBehaviour {
+		public Wall wallA;
+		public Wall wallB;
+		public Shooter shooterA;
+		public Shooter shooterB;
+
+		
+	}
+}
---
+++ Assets/Script/Component/Wall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Component {
+	using Utility;
+
+	public class Wall : MonoBehaviour {
+		public Transform barTransform;
+		private float target;
+		private Timer timer;
+		private Vector3 localScale;
+
+		void Awake() {
+			this.timer = new Timer ();
+		}
+
+		void FixedUpdate() {
+			if (!this.timer.isRunning) {
+				return;
+			}
+
+			this.timer.Update (Time.fixedDeltaTime);
+			this.localScale = this.barTransform.localScale;
+			this.localScale.x = Mathf.Lerp (this.localScale.x, this.target, this.timer.value);
+			this.barTransform.localScale = this.localScale;
+		}
+
+		public void SetLength(float value) {
+			this.target = value;
+			this.timer.Enter (1);
+		}
+	}
+}
